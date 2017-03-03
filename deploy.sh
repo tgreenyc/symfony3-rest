e3700d4da205eebcf21f515a30f734d7d012da06
Tim Green <timsgreen@gmail.com>
2017-03-03 12:42:31 -0500

    make sure deploy script vendors dependencies

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 APPNAME=symfony3-rest
+COMPOSER=`which composer`
+
+if [ ! -d vendor ]; then
+  if [ -z $COMPOSER ]; then
+    echo '[ERROR]: Please install composer first'
+    exit 1
+  else
+    $COMPOSER install
+  fi
+fi
 
 cf push
 cf run-task $APPNAME "bin/console doctrine:schema:update --force"
