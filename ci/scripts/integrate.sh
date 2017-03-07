9bc4280aa8f29c64bbc783891cdbc2ed2de8ac67
Tim Green <timsgreen@gmail.com>
2017-03-07 15:18:16 -0500

    load a temporary database for api testing

--- ci/scripts/integrate.sh
+++ ci/scripts/integrate.sh
@@ -3,4 +3,13 @@ set -e
 
 cd git-repo
 composer install
+mkdir -p var/data
+if [ $? -ne 0 ]; then
+  echo "[ERROR]: unable to create database directory."
+  exit 1
+fi
+
+php bin/console doctrine:database:create -e dev
+php bin/console doctrine:schema:update --force
+php bin/console doctrine:fixtures:load
 vendor/bin/phpunit -vvv tests/AppBundle/Controller/UserControllerTest.php
