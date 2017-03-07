31ed93de7a7ad6356d11a6cb051e1190c5480961
Tim Green <timsgreen@gmail.com>
2017-03-07 17:02:41 -0500

    add sample cf deployment

--- ci/scripts/integrate.sh
+++ ci/scripts/integrate.sh
@@ -13,4 +13,5 @@ fi
 php bin/console doctrine:database:create -e dev
 php bin/console doctrine:schema:update --force
 php bin/console doctrine:fixtures:load -n
+
 vendor/bin/phpunit tests/AppBundle/Controller/UserControllerTest.php
