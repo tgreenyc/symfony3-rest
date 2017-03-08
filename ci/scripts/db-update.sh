72a1def177435bdaeaa6fdbe5cc8c16bc1caf34c
Tim Green <timsgreen@gmail.com>
2017-03-07 19:39:54 -0500

    ignore self signed cert

--- ci/scripts/db-update.sh
+++ ci/scripts/db-update.sh
@@ -3,6 +3,6 @@ set -e
 
 cd git-repo
 
-cf login -a $CF_API -u $CF_USER -p $CF_PASS -o $CF_TEST_ORG -s $CF_TEST_SPACE
+cf login -a $CF_API -u $CF_USER -p $CF_PASS -o $CF_TEST_ORG -s $CF_TEST_SPACE --skip-ssl-validation
 cf run-task $APPNAME "bin/console doctrine:schema:update --force"
 cf run-task $APPNAME "bin/console doctrine:fixtures:load -n"
